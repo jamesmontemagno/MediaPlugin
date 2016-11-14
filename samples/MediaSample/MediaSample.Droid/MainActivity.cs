f12b764ecccba21dda9d3550d3bebb72a4cbe563
James Montemagno <james.montemagno@gmail.com>
2016-11-13 16:06:47 -0800

    Update sample to 2.6.1 of plugin

--- samples/MediaSample/MediaSample.Droid/MainActivity.cs
+++ samples/MediaSample/MediaSample.Droid/MainActivity.cs
@@ -6,19 +6,26 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 
 namespace MediaSample.Droid
 {
-  [Activity(Label = "MediaSample", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-  public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-  {
-    protected override void OnCreate(Bundle bundle)
+    [Activity(Label = "MediaSample", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
-      base.OnCreate(bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-      global::Xamarin.Forms.Forms.Init(this, bundle);
-      LoadApplication(new App());
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
-  }
 }
 
--- samples/MediaSample/MediaSample.Droid/Properties/AssemblyInfo.cs
+++ samples/MediaSample/MediaSample.Droid/Properties/AssemblyInfo.cs
@@ -32,3 +32,6 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+[assembly: UsesFeature("android.hardware.camera", Required = false)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = false)]
