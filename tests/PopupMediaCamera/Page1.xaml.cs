8b06e59396400b883aac8b50f3ceeacc5a367e54
James Montemagno <james.montemagno@gmail.com>
2016-11-23 12:02:52 -0800

    Add pop up tests

---
+++ tests/PopupMediaCamera/Page1.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+
+namespace PopupMediaCamera
+{
+    public partial class Page1
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_OpenPopupPageTapped(object sender, EventArgs e)
+        {
+            Navigation.PushPopupAsync(new PagePopup());
+        }
+    }
+}
---
+++ tests/PopupMediaCamera/PagePopup.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace PopupMediaCamera
+{
+    public partial class PagePopup
+    {
+        public PagePopup()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_StartCameraTapped(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                Debug.WriteLine("No Camera", "No camera avaialble.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                Directory = "InventoryManagement",
+                Name = "item.jpg",
+                PhotoSize = PhotoSize.Medium,
+                CompressionQuality = 42,
+                SaveToAlbum = false,
+                DefaultCamera = CameraDevice.Rear,
+
+            });
+            if (file == null)
+                return;
+        }
+    }
+}
