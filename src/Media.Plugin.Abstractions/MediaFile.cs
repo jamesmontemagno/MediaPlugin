828694e2d4bcf67b491c6eb881db44daf0b96451
James Montemagno <james.montemagno@gmail.com>
2017-06-21 17:18:02 -0700

    More cleanup

--- src/Media.Plugin.Abstractions/MediaFile.cs
+++ src/Media.Plugin.Abstractions/MediaFile.cs
@@ -1,20 +1,4 @@
-﻿//
-//  Copyright 2011-2013, Xamarin Inc.
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
-//        http://www.apache.org/licenses/LICENSE-2.0
-//
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
-//
-
-using System;
+﻿using System;
 using System.IO;
 
 
@@ -23,16 +7,16 @@ namespace Plugin.Media.Abstractions
     /// <summary>
     /// Media file representations
     /// </summary>
-    public sealed class MediaFile
-      : IDisposable
+    public sealed class MediaFile : IDisposable
     {
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="path"></param>
-        /// <param name="streamGetter"></param>
-        public MediaFile(string path, Func<Stream> streamGetter, string albumPath = null)
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="path"></param>
+		/// <param name="streamGetter"></param>
+		/// <param name="albumPath"></param>
+		public MediaFile(string path, Func<Stream> streamGetter, string albumPath = null)
         {
             this.streamGetter = streamGetter;
             this.path = path;
--- src/Media.Plugin.Abstractions/MediaFileNotFoundException.cs
+++ src/Media.Plugin.Abstractions/MediaFileNotFoundException.cs
@@ -1,32 +1,27 @@
-﻿//
-//  Copyright 2011-2013, Xamarin Inc.
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
-//        http://www.apache.org/licenses/LICENSE-2.0
-//
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
-//
-
-using System;
+﻿using System;
 
 namespace Plugin.Media.Abstractions
 {
-    public class MediaFileNotFoundException
-      : Exception
+	/// <summary>
+	/// 
+	/// </summary>
+    public class MediaFileNotFoundException : Exception
     {
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="path"></param>
         public MediaFileNotFoundException(string path)
           : base("Unable to locate media file at " + path)
         {
             Path = path;
         }
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="path"></param>
+		/// <param name="innerException"></param>
         public MediaFileNotFoundException(string path, Exception innerException)
           : base("Unable to locate media file at " + path, innerException)
         {
