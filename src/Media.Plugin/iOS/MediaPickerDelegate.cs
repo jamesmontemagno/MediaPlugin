402e0058681cf4a2e95279dba676a762da363d2e
Jonathan Goldberger <github@johnnygold.com>
2019-10-07 16:30:59 -1000

    Fixes issue #264
    Fixes issue #264 Png image is being converted to jpg when selected from
    gallery in case of iOS
    - Checks for the file extension when first getting the photo info after
    user picks it
    - Sets a Static variable, photoType, with “png” or “jpg” depending on
    photo type retrieved from Photos app.
    - Uses that static variable, photoType, to determine the photo type to
    save and return.
    - This preserves any png transparency that may have been in the PNG
    file saved in the photos app.

--- src/Media.Plugin/iOS/MediaPickerDelegate.cs
+++ src/Media.Plugin/iOS/MediaPickerDelegate.cs
@@ -20,6 +20,8 @@ namespace Plugin.Media
 {
 	internal class MediaPickerDelegate : UIImagePickerControllerDelegate
 	{
+		static string photoType;
+
 		internal MediaPickerDelegate(UIViewController viewController, UIImagePickerControllerSourceType sourceType,
 			StoreCameraMediaOptions options, CancellationToken token)
 		{
@@ -288,8 +290,10 @@ namespace Plugin.Media
 		{
 			var image = (UIImage)info[UIImagePickerController.EditedImage] ?? (UIImage)info[UIImagePickerController.OriginalImage];
 
-            if (image == null)
+			if (image == null)
                 return null;
+
+			photoType = ((info[UIImagePickerController.ReferenceUrl] as NSUrl).PathExtension == "PNG") ? "png" : "jpg";
 
 			var path = GetOutputPath(MediaImplementation.TypeImage,
 				options.Directory ?? ((IsCaptured) ? string.Empty : "temp"),
@@ -384,7 +388,7 @@ namespace Plugin.Media
 			}
 
 			//iOS quality is 0.0-1.0
-			var quality = (options.CompressionQuality / 100f);
+			var quality = photoType == "jpg" ? (options.CompressionQuality / 100f) : 0f;
 			var savedImage = false;
 			if (meta != null)
 				savedImage = SaveImageWithMetadata(image, quality, meta, path);
@@ -392,7 +396,7 @@ namespace Plugin.Media
 			if (!savedImage)
 			{
 				var finalQuality = quality;
-				var imageData = image.AsJPEG(finalQuality);
+				var imageData = photoType == "jpg" ? image.AsJPEG(finalQuality) : image.AsPNG();
 
 				//continue to move down quality , rare instances
 				while (imageData == null && finalQuality > 0)
@@ -474,7 +478,7 @@ namespace Plugin.Media
 			try
 			{
 				var finalQuality = quality;
-				var imageData = image.AsJPEG(finalQuality);
+				var imageData = photoType == "jpg" ? image.AsJPEG(finalQuality) : image.AsPNG();
 
 				//continue to move down quality , rare instances
 				while (imageData == null && finalQuality > 0)
@@ -628,7 +632,7 @@ namespace Plugin.Media
 			if (string.IsNullOrWhiteSpace(name))
 			{
 				if (type == MediaImplementation.TypeImage)
-					name = $"IMG_{postpendName}.jpg";
+					name = photoType == "jpg" ? $"IMG_{postpendName}.jpg" : $"IMG_{postpendName}.png";
 				else
 					name = $"VID_{postpendName}.mp4";
 			}
@@ -763,8 +767,8 @@ namespace Plugin.Media
 				}
 			}
 
-			var finalQuality = compressionQuality / 100f;
-			var imageData = imageToReturn.AsJPEG(finalQuality);
+			var finalQuality = photoType == "jpg" ? (compressionQuality / 100f) : 0f;
+			var imageData = photoType == "jpg" ? imageToReturn.AsJPEG(finalQuality) : imageToReturn.AsPNG();
 			//continue to move down quality , rare instances
 			while (imageData == null && finalQuality > 0)
 			{
